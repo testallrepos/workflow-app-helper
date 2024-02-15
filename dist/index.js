085a4dd2376d4d763066ec1929ad7a6505488ce3
Mallikarjunp <mallik.pulluru@gmail.com>
2024-02-15 16:50:05 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -29330,7 +29330,11 @@ async function preparePipelineResults(inputs) {
     });
     core.info(`Mitigated policy findings: ${mitigatedPolicyFindings.length}`);
     const filteredFindingsArray = findingsArray.filter((finding) => {
+         core.info(`findingDATAAA    ${finding.files.source_file}  `);  
         return !mitigatedPolicyFindings.some((mitigatedFinding) => {
+            
+              core.info(`mitigatedFindingDATA    ${mitigatedFinding.finding_details}`);  
+            
              core.info(`test ${finding.files.source_file.file}   ${mitigatedFinding.finding_details.file_path}`);  
                core.info(` cwe.id ${finding.cwe_id}   ${mitigatedFinding.finding_details.cwe.id}`); 
          core.info(` LINE_NUMBER_SLOP.id ${finding.files.source_file.line - mitigatedFinding.finding_details.file_line_number}   ${LINE_NUMBER_SLOP}`); 
